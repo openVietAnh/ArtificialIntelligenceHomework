a9cb038404d7aa8bc7aa3c6450a3847cd5b44e8c
vietanhtran2710 <vietanhtran.uet@gmail.com>
2021-10-09 20:42:20 +0700

    Optimized

--- week5/tictactoe3x3.py
+++ week5/tictactoe3x3.py
@@ -7,25 +7,24 @@ def print_board(board):
         print("|" + "|".join(row) + "|")
         print("-" * 7)
 
-def get_terminal(board):
-    global turn_count
+def get_terminal(board, turn_count):
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] != " ":
+            return -10 if board[i][0] == "X" else 10
+        if board[0][i] == board[1][i] == board[2][i] and board[0][i] != " ":
+            return -10 if board[0][i] == "X" else 10
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != " ":
+        return -10 if board[0][0] == "X" else 10
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != " ":
+        return -10 if board[0][2] == "X" else 10
     if turn_count == 9:
         return 0
-    else:
-        for i in range(3):
-            if board[i][0] == board[i][1] == board[i][2] and board[i][0] != " ":
-                return -10 if board[i][0] == "X" else 10
-            if board[0][i] == board[1][i] == board[2][i] and board[0][i] != " ":
-                return -10 if board[0][i] == "X" else 10
-        if board[0][0] == board[1][1] == board[2][2] and board[0][0] != " ":
-            return -10 if board[0][0] == "X" else 10
-        if board[0][2] == board[1][1] == board[2][0] and board[0][2] != " ":
-            return -10 if board[0][2] == "X" else 10
-        return 1
+    return 1
 
 def minimax(board, depth, isMaximized):
-    if depth == 9:
-        return get_terminal(board), -1, -1
+    current_state = get_terminal(board, depth)
+    if current_state != 1:
+        return current_state, -1, -1
     value = -11 if isMaximized else 11
     x, y = None, None
     for i in range(3):
@@ -57,13 +56,12 @@ while turn_count < 9:
     i, j = list(map(int, input().split()))
     board[i][j] = 'X'
     turn_count += 1
-    current_state = get_terminal(board)
-    print(current_state)
+    current_state = get_terminal(board, turn_count)
     if current_state == 1:
         _, i, j = get_next_move(board)
         board[i][j] = 'O'
         turn_count += 1
-        if get_terminal(board) == 10:
+        if get_terminal(board, turn_count) == 10:
             print("O XON")
             break
     else:
